af93f9c8ddf53720677abf1b5ffdf82d8e2d411d
Takashi SASAKI <takashi316@gmail.com>
2017-11-04 19:12:56 +0900

    support dsa, ecdsa and ed25519

--- listkeys.py
+++ listkeys.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 import os,subprocess,re,tempfile,socket,uuid,datetime
 
-def isClearRsaPrivateKey(filename: str):
+def isClearKeyFile(filename: str):
   f = open(filename)
   lines = f.readlines()
-  m = re.compile("BEGIN RSA PRIVATE KEY").search(lines[0])
+  m = re.compile("BEGIN (RSA|EC|DSA|OPENSSH) PRIVATE KEY").search(lines[0])
   if m is None: return False
   m = re.compile("ENCRYPTED").search(lines[1])
   if m is None: return True;
   return False
 
-def isOpenSshPublicKeyFile(filename: str):
+def isPublicKeyFile(filename: str):
   f = open(filename)
   lines = f.readlines()
-  m = re.compile("^ssh-rsa ").search(lines[0])
-  return m is not None
+  m = re.compile("^(ssh-rsa|ssh-dss|ecdsa-sha2-nistp256|ssh-ed25519) ").search(lines[0])
+  if m is not None: return True
+  return False
 
-def rsaPrivateKeyFileToRsaPublicKey(filename:str):
-  opensslRsa = subprocess.run(["openssl", "rsa", "-pubout", "-RSAPublicKey_out", "-in", filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  rsaPublicKey = opensslRsa.stdout.decode("ascii")
+def rsaPrivateKeyFileToRsaPublicKey(rsaPrivateKeyFile:str):
+  openssl = subprocess.run(["openssl", "rsa", "-pubout", "-RSAPublicKey_out", "-in", rsaPrivateKeyFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  rsaPublicKey = openssl.stdout.decode("ascii")
   return rsaPublicKey
 
 
-def rsaPublicKeyToOpenSshPublicKey(rsaPublicKey:str):
+def rsaPublicKeyToPublicKey(rsaPublicKey:str):
   temporaryFile = tempfile.NamedTemporaryFile(delete=False)
   temporaryFile.write(rsaPublicKey.encode("ascii"))
   temporaryFile.close()
@@ -30,26 +31,32 @@ def rsaPublicKeyToOpenSshPublicKey(rsaPublicKey:str):
   openSshPublicKey = sshKeyGenProcess.stdout.decode("ascii")
   return openSshPublicKey
 
-def openSshPublicKeyToFingerprint(openSshPublicKey:str):
-  sshKeyGenFingerprint = subprocess.Popen(["ssh-keygen", "-l", "-E", "md5", "-f", "-"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  fingerprintLine = sshKeyGenFingerprint.communicate(openSshPublicKey.encode("ascii"))[0].decode("ascii")
+def publicKeyToFingerprint(publicKey:str):
+  sshKeygen = subprocess.Popen(["ssh-keygen", "-l", "-E", "md5", "-f", "-"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  fingerprintLine = sshKeygen.communicate(publicKey.encode("ascii"))[0].decode("ascii")
   return parseFingerprintLine(fingerprintLine)
 
-def openSshPublicKeyFileToFingerprint(filename:str):
-  return openSshPublicKeyToFingerprint(open(filename).read())
+def privateKeyFileToPublicKey(privateKeyFile:str):
+  sshKeygen = subprocess.Popen(["ssh-keygen", "-y", "-f", privateKeyFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  publicKey = sshKeygen.communicate()[0].decode("ascii")
+  return publicKey
 
-def rsaPrivateKeyFileToFingerprint(filename: str):
-  rsaPublicKey = rsaPrivateKeyFileToRsaPublicKey(filename)
-  openSshPublicKey = rsaPublicKeyToOpenSshPublicKey(rsaPublicKey)
-  fingerprint = openSshPublicKeyToFingerprint(openSshPublicKey)
+def publicKeyFileToFingerprint(publicKeyFile:str):
+  return publicKeyToFingerprint(open(publicKeyFile).read())
+
+def rsaPrivateKeyFileToFingerprint(rsaPrivateKeyFile: str):
+  rsaPublicKey = rsaPrivateKeyFileToRsaPublicKey(rsaPrivateKeyFile)
+  publicKey = rsaPublicKeyToPublicKey(rsaPublicKey)
+  fingerprint = publicKeyToFingerprint(publicKey)
   return fingerprint
 
 def parseFingerprintLine(fingerprintLine:str):
-  m = re.compile("^([0-9]+) +MD5:([0-9A-Fa-f:]+) +(.*)$").match(fingerprintLine)
+  m = re.compile("^([0-9]+) +MD5:([0-9A-Fa-f:]+) +(.*) +\(([0-9A-Z]+)\)$").match(fingerprintLine)
   bits = m.group(1)
   md5 = m.group(2)
   comment = m.group(3)
-  return [bits, md5, comment]
+  algorithm = m.group(4)
+  return [bits, md5, comment, algorithm]
 
 if __name__ == "__main__":
   result = []
@@ -66,16 +73,18 @@ if __name__ == "__main__":
     d["absolutePath"] = os.path.abspath(filename)
     try: 
       fingerprint = None
-      if isClearRsaPrivateKey(filename):
-        fingerprint = rsaPrivateKeyFileToFingerprint(filename)
+      if isClearKeyFile(filename):
+        publicKey = privateKeyFileToPublicKey(filename)
+        fingerprint = publicKeyToFingerprint(publicKey)
         d["private"] = True
-      elif isOpenSshPublicKeyFile(filename):
-        fingerprint = openSshPublicKeyFileToFingerprint(filename)
+      elif isPublicKeyFile(filename):
+        fingerprint = publicKeyFileToFingerprint(filename)
         d["private"] = False
       else: continue
       d["bits"] = fingerprint[0]
       d["md5"] = fingerprint[1]
       d["comment"] = fingerprint[2]
+      d["algorithm"] = fingerprint[3]
       print(d)
     except UnicodeDecodeError as e:
       continue
